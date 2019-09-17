c7be9e9f7316ae2e0ba2c4aa3e186f82174b5ece
AussieAdapt <49267121+AussieAdapt@users.noreply.github.com>
2019-09-17 20:15:03 +0800

    Update killerwhale.js

--- commands/fun/killerwhale.js
+++ commands/fun/killerwhale.js
@@ -8,7 +8,7 @@ const {
     description: "shows server info",
     run: async (client, message, args) => {
 
-        const res = await fetch("https://and-here-is-my-code.glitch.me/killerwhale")
+        const res = await fetch("https://and-here-is-my-code.glitch.me/img/killerwhale")
         .then(res => res.json())
         .then(json => {
           return json
