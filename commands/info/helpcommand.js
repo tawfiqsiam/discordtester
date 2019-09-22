695ff08f9d07c8703746478de561ba8a1f65b2a2
AussieAdapt <49267121+AussieAdapt@users.noreply.github.com>
2019-09-22 20:27:51 +0800

    Update helpcommand.js

--- commands/info/helpcommand.js
+++ commands/info/helpcommand.js
@@ -4,37 +4,39 @@ module.exports = {
     name: "help",
     description: "shows server info",
     run: async (client, message, args) => {
-        
+
     let sicon = message.guild.iconURL;
     let serverembed = new Discord.RichEmbed()
     .setDescription("random images\n"+
-    "• whale | sends image of a whale \n"+
-    "• bird | sends a random image of a bird\n"+
-    "• giraffe | sends a random image of a giraffe\n"+
-	"• elephant | sends a random image of a elephant\n"+
-	"• turtle | sends a random image of a turtle\n"+
-	"• killerwhale | sends a random image of a killerwhale\n"+
-    "• koala | sends a random image of a koala \n"+
-    "• dog | sends a random image of a dog \n"+
-    "• fox | sends a random image of a fox \n"+
-    "• koala | sends a random image of a koala \n"+
-    "• panda | sends a random image of a panda \n"+
-    "• hug | sends a random gif of a people hugging\n "+
-    "• meme | sends a random meme \n"+
-    " Other commands \n"+
-    "• 8ball | wana know an awnser ask away \n"+
-    "• serverinfo | Post info about the server \n"+
-    "• hug | sends a random gif of a people hugging \n"+
-    "• wink | sends a random gif of someone winking \n"+
-    "• pat | sends a random gif of someone pating \n"+
-    "admin commands\n"+
-    "• kick | kick a user \n"+
-    "• ban | ban a user \n"+
-    "• purge | purge messages"
+  "• whale | sends image of a whale \n"+
+  "• bird | sends a random image of a bird\n"+
+  "• giraffe | sends a random image of a giraffe\n"+
+  "• elephant | sends a random image of a elephant\n"+
+  "• turtle | sends a random image of a turtle\n"+
+  "• killerwhale | sends a random image of a killerwhale\n"+
+  "• koala | sends a random image of a koala \n"+
+  "• dog | sends a random image of a dog \n"+
+  "• fox | sends a random image of a fox \n"+
+  "• koala | sends a random image of a koala \n"+
+  "• panda | sends a random image of a panda \n"+
+  "• hug | sends a random gif of a people hugging\n "+
+  "• meme | sends a random meme \n"+
+  " Other commands \n"+
+  "• 8ball | wana know an awnser ask away \n"+
+  "• serverinfo | Post info about the server \n"+
+  "• hug | sends a random gif of a people hugging \n"+
+  "• wink | sends a random gif of someone winking \n"+
+  "• pat | sends a random gif of someone pating \n"+
+  "admin commands\n"+
+  "• kick | kick a user \n"+
+  "• ban | ban a user \n"+
+  "• purge | purge messages"
   );
 
 
     message.channel.send(serverembed);
-   
+
+
+}
 
 }
