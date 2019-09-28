3cf9ca1197b751cd078d5bf4a94d6d19c1a4cb2a
AussieAdapt <49267121+AussieAdapt@users.noreply.github.com>
2019-09-28 20:48:29 +0800

    Create lion.js

---
+++ commands/fun/lion.js
@@ -0,0 +1,32 @@
+const {
+    RichEmbed
+  } = require('discord.js');
+  const fetch = require("node-fetch");
+  
+  module.exports = {
+    name: "lion",
+    description: "shows server info",
+    run: async (client, message, args) => {
+
+        const res = await fetch("https://and-here-is-my-code.glitch.me/img/lion")
+        .then(res => res.json())
+        .then(json => {
+          return json
+        });
+
+        
+       
+      
+      console.log(res);
+  
+      const embed = new RichEmbed()
+        .setColor("RANDOM")
+        .setImage(res.Link)
+
+
+        
+      
+      message.channel.send(embed);
+      }
+
+}
