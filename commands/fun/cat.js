22df14b9e105a8580ac493893d18f8d9e29ec764
AussieAdapt <49267121+AussieAdapt@users.noreply.github.com>
2019-10-11 01:05:33 +0800

    Update cat.js

--- commands/fun/cat.js
+++ commands/fun/cat.js
@@ -8,7 +8,7 @@ const {
     description: "shows server info",
     run: async (client, message, args) => {
 
-        const res = await fetch("https://some-random-api.ml/img/cat")
+        const res = await fetch("https://and-here-is-my-code.glitch.me/img/cat")
         .then(res => res.json())
         .then(json => {
           return json
