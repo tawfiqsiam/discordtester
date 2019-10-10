5bf9a5f4b557cca9c40caa27a0bf76c9e08d34a9
AussieAdapt <49267121+AussieAdapt@users.noreply.github.com>
2019-10-11 01:13:16 +0800

    Update dog.js

--- commands/fun/dog.js
+++ commands/fun/dog.js
@@ -8,23 +8,25 @@ const {
     description: "shows server info",
     run: async (client, message, args) => {
 
-        const res = await fetch("https://and-here-is-my-code.glitch.me/dog")
+        const res = await fetch("https://and-here-is-my-code.glitch.me/img/dog")
         .then(res => res.json())
         .then(json => {
           return json
         });
+
+        
+       
       
       console.log(res);
-      
+  
       const embed = new RichEmbed()
         .setColor("RANDOM")
-        .setImage(res.status == "success" ? res.message : "https://i.gyazo.com/4e7a4b6834400626ecf0a45d370e1f20.png");
+        .setImage(res.Link)
+
+
+        
       
       message.channel.send(embed);
       }
 
 }
-
-
-
- 
