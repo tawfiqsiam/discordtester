1d14b86717d3ce337c72e2628fcc5292f4590909
AussieAdapt <49267121+AussieAdapt@users.noreply.github.com>
2019-10-11 00:50:41 +0800

    shark command

---
+++ commands/fun/shark.js
@@ -0,0 +1,32 @@
+const {
+    RichEmbed
+  } = require('discord.js');
+  const fetch = require("node-fetch");
+  
+  module.exports = {
+    name: "shark",
+    description: "shows server info",
+    run: async (client, message, args) => {
+
+        const res = await fetch("https://and-here-is-my-code.glitch.me/img/shark")
+        .then(res => res.json())
+        .then(json => {
+          return json
+        });
+
+        
+       
+      
+      console.log(res);
+  
+      const embed = new RichEmbed()
+        .setColor("RANDOM")
+        .setImage(res.Link)
+
+
+        
+      
+      message.channel.send(embed);
+      }
+
+}
