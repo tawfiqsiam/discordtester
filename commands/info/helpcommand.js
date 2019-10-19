be0854a525d8cdb1c319f12fe3018182f7633992
AussieAdapt <49267121+AussieAdapt@users.noreply.github.com>
2019-10-19 20:39:42 +0800

    Update helpcommand.js

--- commands/info/helpcommand.js
+++ commands/info/helpcommand.js
@@ -13,24 +13,35 @@ module.exports = {
   "• giraffe | sends a random image of a giraffe\n"+
   "• elephant | sends a random image of a elephant\n"+
   "• turtle | sends a random image of a turtle\n"+
+   "• horse | sends image of a horse \n"+                 
   "• killerwhale | sends a random image of a killerwhale\n"+
   "• koala | sends a random image of a koala \n"+
   "• dog | sends a random image of a dog \n"+
   "• fox | sends a random image of a fox \n"+
   "• koala | sends a random image of a koala \n"+
   "• panda | sends a random image of a panda \n"+
+  "• shark | sends image of a shark \n"+
+  "• fish | sends image of a spider \n"+ 
+  "• lizard | sends image of a lizard \n"+                  
+   "• spider | sends image of a shark \n"+                 
+  "• car | sends image of a car \n"+
+  "• rocket | sends image of a rocket \n"+
   "• hug | sends a random gif of a people hugging\n "+
   "• meme | sends a random meme \n"+
+  "• trump | sends image of trump \n"+
   " Other commands \n"+
   "• 8ball | wana know an awnser ask away \n"+
   "• serverinfo | Post info about the server \n"+
   "• hug | sends a random gif of a people hugging \n"+
   "• wink | sends a random gif of someone winking \n"+
   "• pat | sends a random gif of someone pating \n"+
+  "• coinflip | heads or tails what will it be \n"+
   "admin commands\n"+
   "• kick | kick a user \n"+
+  "• report | report a user to the owner and get them gone\n"+
   "• ban | ban a user \n"+
   "• purge | purge messages"
+    "my website  https://and-here-is-my-code.glitch.me\n"+
   );
 
 
