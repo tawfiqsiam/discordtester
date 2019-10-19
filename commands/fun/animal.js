ddd366ee40f323ca54768420acd2d10cbdcec4ba
AussieAdapt <49267121+AussieAdapt@users.noreply.github.com>
2019-10-19 22:46:30 +0800

    Create animal.js

---
+++ commands/fun/animal.js
@@ -0,0 +1,31 @@
+const {
+    RichEmbed
+  } = require('discord.js');
+  const fetch = require("node-fetch");
+  
+  module.exports = {
+    name: "animal",
+    description: "shows server info",
+    run: async (client, message, args) => {
+
+        const res = await fetch("http://and-here-is-my-code.glitch.me/img/animal")
+        .then(res => res.json())
+        .then(json => {
+          return json
+        });
+
+        
+       
+      
+      console.log(res);
+  
+      const embed = new RichEmbed()
+        .setColor("RANDOM")
+
+        .setImage(res.image);
+        
+      
+      message.channel.send(embed);
+      }
+
+}
