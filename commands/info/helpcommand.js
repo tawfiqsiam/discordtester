bc6e489aa353c0d7e18acde0a3ced7b9b798efd7
AussieAdapt <49267121+AussieAdapt@users.noreply.github.com>
2019-10-19 20:40:51 +0800

    Update helpcommand.js

--- commands/info/helpcommand.js
+++ commands/info/helpcommand.js
@@ -40,8 +40,8 @@ module.exports = {
   "• kick | kick a user \n"+
   "• report | report a user to the owner and get them gone\n"+
   "• ban | ban a user \n"+
-  "• purge | purge messages"
-    "my website  https://and-here-is-my-code.glitch.me\n"+
+  "• purge | purge messages"+
+    "my website  https://and-here-is-my-code.glitch.me\n"
   );
 
 
