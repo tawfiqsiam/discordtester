7170b825170c4336fe1cd9e5755e0d804d9fb787
AussieAdapt <49267121+AussieAdapt@users.noreply.github.com>
2019-10-21 18:30:22 +0800

    Update monkey.js

--- commands/fun/monkey.js
+++ commands/fun/monkey.js
@@ -4,7 +4,7 @@ const {
   const fetch = require("node-fetch");
   
   module.exports = {
-    name: "bird",
+    name: "monkey",
     description: "shows server info",
     run: async (client, message, args) => {
 
